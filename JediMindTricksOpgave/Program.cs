7a057c8a5b68ad6268c1bb7c36e3a97cee783458
patrick jesssen <patrick-j1@hotmail.dk>
2021-02-22 18:14:55 +0100

    Add project files.

---
+++ JediMindTricksOpgave/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JediMindTricksOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("Patrick", 25);
+            dict["navn"] = 90;
+
+            Console.WriteLine(dict.ElementAt(0));
+
+
+            Dictionary<string, bool> characters = new Dictionary<string, bool>()
+            {
+                { "Luke", true },
+                { "Han", false },
+                { "Chewbacca", false }
+            };
+
+            characters.Remove("Han");
+
+            foreach (KeyValuePair<string, bool> item in characters)
+            {
+                Console.WriteLine(item.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
